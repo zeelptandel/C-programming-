cb72e80c66cf8d18429375cd1e4874fd72984bb2
zeelptandel <zeelptandel21@gmail.com>
2025-08-22 10:55:36 +0530

    Add files via upload

---
+++ math functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=10,b=4;
+    float c=10,d=4;
+    printf("sum of numbers is ");
+    printf("%d\n",a+b);
+    printf("difference of numbers is ");
+    printf("%d\n",a-b);
+    printf("product of numbers is ");
+    printf("%d\n",a*b);
+    printf("division of numbers is ");
+    printf("%f\n",c/d);
+    return 0;
+}
+
