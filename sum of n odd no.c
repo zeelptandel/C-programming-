14d5d3887d3444f464b22d1214e8c0d08eba25eb
zeelptandel <zeelptandel21@gmail.com>
2025-08-29 10:13:26 +0530

    Add files via upload

---
+++ sum of n odd no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n , sum=0;
+printf("Enter the number of odd no's : ");
+scanf("%d",&n);
+sum = n*n;
+printf("The sum of 1st %d odd numbers is : %d\n",n,sum);
+return 0;
+
+
+}
