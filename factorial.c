58f08047fc8b8d8ab90947cea212e1ec2c11e6b8
zeelptandel <zeelptandel21@gmail.com>
2025-11-20 19:10:41 +0530

    factorial.c

--- factorial.c
+++ factorial.c
@@ -1,23 +1,14 @@
-#include<stdio.h>
-int factorial(int a){
-int result = 1;
-for(int i=1 ; i<=a ; i++){
-result *= i;
-}
-return result ;
-}
-int main()
-{
-int a;
-printf("Enter a number: ");
-scanf("%d",&a);
-if (a==0){
-        printf("1");
-    } else if(a<0){
-        printf("0");
-    } else{
-        printf("factorial of %d is: %d", a,factorial(a));
-    }
+#include <stdio.h>
 
+int main() {
+    int n, f = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+        f *= i;
+
+    printf("Factorial = %d", f);
     return 0;
 }
